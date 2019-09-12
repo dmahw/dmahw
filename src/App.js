2ac42914552aa7cb7288f704a61bc80436e2dedd
dmahw <dmahw@uvic.ca>
2019-09-12 12:37:22 -0700

    school changes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import NavigationBar from './components/NavigationBar'
 function App() {
   return (
     <div className="App">
+      <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      </head> 
       <header className="App-header">
         <NavigationBar></NavigationBar>
       </header>
       <HeroHeader></HeroHeader>
-
     </div>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,13 +3,44 @@ import './css/NavigationBar.css';
 
 function NavigationBar() {
     return (
-        <ul className="navBar">
-            <li><a class="active" href="home">Home</a></li>
-            <li><a href="about-me">About Me</a></li>
-            <li><a href="work-experience">Work Experience</a></li>
-            <li><a href="projects">Projects</a></li>
-            <li><a href="contact">Contact</a></li>
-        </ul> 
+        <div className="wrapper">
+            <head>
+                <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+            </head>
+            <nav>
+                <ul className="navBar">
+                    <li><a class="active" href="home">Home</a></li>
+                    <li><a href="about-me">About Me</a></li>
+                    <li><a href="work-experience">Work Experience</a></li>
+                    <li><a href="projects">Projects</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul> 
+            </nav>
+
+            <section>
+                <article>
+                    <h4>Section 1</h4>
+                </article>
+            </section>
+
+            <section>
+                <article>
+                    <h4>Section 2</h4>
+                </article>
+            </section>
+
+            <section>
+                <article>
+                    <h4>Section 3</h4>
+                </article>
+            </section>
+
+            <section>
+                <article>
+                    <h4>Section 4</h4>
+                </article>
+            </section>
+        </div>
     )
 }
 
